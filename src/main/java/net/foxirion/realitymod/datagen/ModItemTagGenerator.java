7f43f2a09a7755f1ea9afdb715350a1279f33d28
Foxirion <dariustam@yahoo.com>
2024-09-23 00:52:31 +0300

    - Palm blocks can now be used as any type of wood(to make charcoal, used as fuel, crafting sticks, chests and other items that use the "plank" or "logs_that_burn" or "log" id

--- src/main/java/net/foxirion/realitymod/datagen/ModItemTagGenerator.java
+++ src/main/java/net/foxirion/realitymod/datagen/ModItemTagGenerator.java
@@ -1,9 +1,12 @@
 package net.foxirion.realitymod.datagen;
 
 import net.foxirion.realitymod.RealityMod;
+import net.foxirion.realitymod.block.ModBlocks;
+import net.foxirion.realitymod.util.ModTags;
 import net.minecraft.core.HolderLookup;
 import net.minecraft.data.PackOutput;
 import net.minecraft.data.tags.ItemTagsProvider;
+import net.minecraft.tags.ItemTags;
 import net.minecraft.world.level.block.Block;
 import net.minecraftforge.common.data.ExistingFileHelper;
 import org.jetbrains.annotations.Nullable;
@@ -19,6 +22,22 @@ public class ModItemTagGenerator extends ItemTagsProvider {
 
     @Override
     protected void addTags(HolderLookup.Provider pProvider) {
+        this.tag(ItemTags.LEAVES)
+                .add(ModBlocks.PALM_LEAVES.get().asItem());
 
+        this.tag(ItemTags.PLANKS)
+                .add(ModBlocks.PALM_PLANKS.get().asItem());
+
+        this.tag(ItemTags.LOGS_THAT_BURN)
+                .add(ModBlocks.PALM_LOG.get().asItem(),
+                        ModBlocks.PALM_WOOD.get().asItem(),
+                        ModBlocks.STRIPPED_PALM_LOG.get().asItem(),
+                        ModBlocks.STRIPPED_PALM_WOOD.get().asItem());
+
+        this.tag(ModTags.Items.PALM_LOGS)
+                .add(ModBlocks.PALM_LOG.get().asItem(),
+                        ModBlocks.PALM_WOOD.get().asItem().asItem(),
+                        ModBlocks.STRIPPED_PALM_LOG.get().asItem(),
+                        ModBlocks.STRIPPED_PALM_WOOD.get().asItem());
     }
 }
