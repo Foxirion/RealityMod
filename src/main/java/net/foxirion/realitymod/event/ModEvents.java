63f204e32a80807ed3b4e5e92b08119e2ae910a9
Foxirion <dariustam@yahoo.com>
2024-09-25 14:46:20 +0300

    - Added ModEvents class - Farmers and Wandering new trades(from Farmer you can buy 4 coconuts, and from wandering trader palm sapling)

---
+++ src/main/java/net/foxirion/realitymod/event/ModEvents.java
@@ -0,0 +1,54 @@
+package net.foxirion.realitymod.event;
+
+import it.unimi.dsi.fastutil.ints.Int2ObjectMap;
+import net.foxirion.realitymod.RealityMod;
+import net.foxirion.realitymod.block.ModBlocks;
+import net.foxirion.realitymod.item.ModItems;
+import net.minecraft.world.entity.npc.VillagerProfession;
+import net.minecraft.world.entity.npc.VillagerTrades;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
+import net.minecraft.world.item.trading.MerchantOffer;
+import net.minecraftforge.event.village.VillagerTradesEvent;
+import net.minecraftforge.event.village.WandererTradesEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+
+import java.util.List;
+
+@Mod.EventBusSubscriber(modid = RealityMod.MOD_ID)
+public class ModEvents {
+
+    //Custom Trades Villagers & Wandering Trader
+
+    //Villagers
+
+    @SubscribeEvent
+    public static void addCustomTrades(VillagerTradesEvent event){
+        //Farmer
+        if (event.getType() == VillagerProfession.FARMER){
+            Int2ObjectMap<List<VillagerTrades.ItemListing>> trades = event.getTrades();
+
+            //Apprentice
+            trades.get(2).add((pTrader, pRandom) -> new MerchantOffer(
+                    new ItemStack(Items.EMERALD, 1),
+                    new ItemStack(ModItems.COCONUT.get(), 4),
+                    12, 5, 0.05f));
+        }
+    }
+
+    //Wandering Traders
+
+    @SubscribeEvent
+    public static void  addCustomWanderingTrades(WandererTradesEvent event){
+        List<VillagerTrades.ItemListing> genericTrades = event.getGenericTrades();
+        List<VillagerTrades.ItemListing> rareTrades = event.getRareTrades();
+
+        genericTrades.add((pTrader, pRandom) -> new MerchantOffer(
+                new ItemStack(Items.EMERALD, 5),
+                new ItemStack(ModBlocks.PALM_SAPLING.get().asItem(), 1),
+                8, 12, 0.2f));
+
+    }
+
+}
