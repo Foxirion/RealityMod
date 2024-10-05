1fdaa7b5fc567ce18e07d14199dd4e725c457036
Foxirion <dariustam@yahoo.com>
2024-10-05 18:08:40 +0300

    - fossil types spawning + loot added

--- src/main/java/net/foxirion/realitymod/worldgen/ModBiomeModifiers.java
+++ src/main/java/net/foxirion/realitymod/worldgen/ModBiomeModifiers.java
@@ -1,4 +1,38 @@
 package net.foxirion.realitymod.worldgen;
 
+import net.foxirion.realitymod.RealityMod;
+import net.minecraft.core.HolderSet;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.data.worldgen.BootstapContext;
+import net.minecraft.resources.ResourceKey;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.tags.BiomeTags;
+import net.minecraft.world.level.levelgen.GenerationStep;
+import net.minecraftforge.common.world.BiomeModifier;
+import net.minecraftforge.common.world.ForgeBiomeModifiers;
+import net.minecraftforge.registries.ForgeRegistries;
+
 public class ModBiomeModifiers {
+    public static final ResourceKey<BiomeModifier> ADD_FOSSIL = registerKey("add_fossil");
+    public static final ResourceKey<BiomeModifier> ADD_NETHER_FOSSIL = registerKey("add_nether_fossil");
+
+    public static void bootstrap (BootstapContext<BiomeModifier> context) {
+        var placedFeatures = context.lookup(Registries.PLACED_FEATURE);
+        var biomes = context.lookup(Registries.BIOME);
+
+        context.register(ADD_FOSSIL, new ForgeBiomeModifiers.AddFeaturesBiomeModifier(
+                biomes.getOrThrow(BiomeTags.IS_OVERWORLD),
+                HolderSet.direct(placedFeatures.getOrThrow(ModPlacedFeatures.FOSSIL_PLACED_KEY)),
+                GenerationStep.Decoration.UNDERGROUND_ORES));
+
+        context.register(ADD_NETHER_FOSSIL, new ForgeBiomeModifiers.AddFeaturesBiomeModifier(
+                biomes.getOrThrow(BiomeTags.IS_NETHER),
+                HolderSet.direct(placedFeatures.getOrThrow(ModPlacedFeatures.NETHER_FOSSIL_PLACED_KEY)),
+                GenerationStep.Decoration.UNDERGROUND_ORES));
+
+    }
+
+    private static ResourceKey<BiomeModifier> registerKey(String name) {
+        return ResourceKey.create(ForgeRegistries.Keys.BIOME_MODIFIERS, new ResourceLocation(RealityMod.MOD_ID, name));
+    }
 }
--- src/main/java/net/foxirion/realitymod/worldgen/ModConfiguredFeatures.java
+++ src/main/java/net/foxirion/realitymod/worldgen/ModConfiguredFeatures.java
@@ -1,20 +1,47 @@
 package net.foxirion.realitymod.worldgen;
 
 import net.foxirion.realitymod.RealityMod;
+import net.foxirion.realitymod.block.ModBlocks;
+import net.foxirion.realitymod.util.ModTags;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.data.worldgen.BootstapContext;
 import net.minecraft.resources.ResourceKey;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.tags.BlockTags;
+import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.levelgen.feature.ConfiguredFeature;
 import net.minecraft.world.level.levelgen.feature.Feature;
 import net.minecraft.world.level.levelgen.feature.configurations.FeatureConfiguration;
+import net.minecraft.world.level.levelgen.feature.configurations.OreConfiguration;
+import net.minecraft.world.level.levelgen.structure.templatesystem.BlockMatchTest;
+import net.minecraft.world.level.levelgen.structure.templatesystem.RuleTest;
+import net.minecraft.world.level.levelgen.structure.templatesystem.TagMatchTest;
+
+import java.util.List;
 
 public class ModConfiguredFeatures {
+    public static final ResourceKey<ConfiguredFeature<?, ?>> OVERWORLD_FOSSIL_KEY = registerKey("overworld_fossil");
+    public static final ResourceKey<ConfiguredFeature<?, ?>> NETHER_FOSSIL_KEY = registerKey("nether_fossil");
+
+    public static void boostrap(BootstapContext<ConfiguredFeature<?, ?>> context) {
+        RuleTest stoneReplaceables = new TagMatchTest(BlockTags.STONE_ORE_REPLACEABLES);
+        RuleTest deepslateReplaceables = new TagMatchTest(BlockTags.DEEPSLATE_ORE_REPLACEABLES);
+        RuleTest netherrackReplacables = new BlockMatchTest(Blocks.NETHERRACK);
+        RuleTest iceReplaceables = new BlockMatchTest(Blocks.PACKED_ICE);
+
+        List<OreConfiguration.TargetBlockState> overworldFossils = List.of(
+                OreConfiguration.target(stoneReplaceables, ModBlocks.FOSSIL.get().defaultBlockState()),
+                OreConfiguration.target(deepslateReplaceables, ModBlocks.DEEPSLATE_FOSSIL.get().defaultBlockState()),
+                OreConfiguration.target(iceReplaceables, ModBlocks.FROZEN_FOSSIL.get().defaultBlockState()));
+
+        register(context, OVERWORLD_FOSSIL_KEY, Feature.ORE, new OreConfiguration(overworldFossils, 4));
+        register(context, NETHER_FOSSIL_KEY, Feature.ORE, new OreConfiguration(netherrackReplacables,
+                ModBlocks.NETHER_FOSSIL.get().defaultBlockState(), 4));
+    }
 
     public static ResourceKey<ConfiguredFeature<?, ?>> registerKey(String name) {
         return ResourceKey.create(Registries.CONFIGURED_FEATURE, new ResourceLocation(RealityMod.MOD_ID, name));
     }
-
     private static <FC extends FeatureConfiguration, F extends Feature<FC>> void register(BootstapContext<ConfiguredFeature<?, ?>> context,
                                                                                           ResourceKey<ConfiguredFeature<?, ?>> key, F feature, FC configuration) {
         context.register(key, new ConfiguredFeature<>(feature, configuration));
