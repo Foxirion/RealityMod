bb139c7a975fc76ce30e27d9b4a67938878b37eb
Foxirion <dariustam@yahoo.com>
2024-10-07 19:21:21 +0300

    desert turtle eggs fix

--- src/main/java/net/foxirion/realitymod/datagen/ModBlockStateProvider.java
+++ src/main/java/net/foxirion/realitymod/datagen/ModBlockStateProvider.java
@@ -2,6 +2,7 @@ package net.foxirion.realitymod.datagen;
 
 import net.foxirion.realitymod.RealityMod;
 import net.foxirion.realitymod.block.ModBlocks;
+import net.foxirion.realitymod.block.custom.DesertTurtleEggBlock;
 import net.foxirion.realitymod.block.custom.ModFlammableRotatedPillarBlock;
 import net.minecraft.core.Direction;
 import net.minecraft.data.PackOutput;
@@ -38,6 +39,8 @@ public class ModBlockStateProvider extends BlockStateProvider {
 
         buttonBlock(((ButtonBlock) ModBlocks.PALM_BUTTON.get()),blockTexture(ModBlocks.PALM_PLANKS.get()));
 
+        createDesertTurtleEggBlockStateAndModel(ModBlocks.DESERT_TURTLE_EGG.get(), "realitymod");
+
         createFossilBlock(ModBlocks.FOSSIL.get(), "fossil", "fossil_front", "fossil_side");
         createFossilBlock(ModBlocks.DEEPSLATE_FOSSIL.get(), "deepslate_fossil", "deepslate_fossil_front", "deepslate_fossil_side");
         createFossilBlock(ModBlocks.FROZEN_FOSSIL.get(), "frozen_fossil", "frozen_fossil_front", "frozen_fossil_side");
@@ -71,6 +74,50 @@ public class ModBlockStateProvider extends BlockStateProvider {
                 "cutout");
 
     }
+
+    public void createDesertTurtleEggBlockStateAndModel(Block desertTurtleEgg, String modId) {
+        var variantBuilder = getVariantBuilder(desertTurtleEgg);
+
+        for (int eggs = 1; eggs <= 4; eggs++) {
+            for (int hatch = 0; hatch <= 2; hatch++) {
+                // Determine the appropriate texture based on hatch level
+                String textureName = switch (hatch) {
+                    case 1 -> "desert_turtle_egg_slightly_cracked";
+                    case 2 -> "desert_turtle_egg_very_cracked";
+                    default -> "desert_turtle_egg";
+                };
+
+                // Construct the model name based on eggs and texture type
+                String modelName = eggs + "_" + textureName;
+
+                // Determine the parent model based on egg count
+                String parentModel = switch (eggs) {
+                    case 1 -> "template_turtle_egg";
+                    case 2 -> "template_two_turtle_eggs";
+                    case 3 -> "template_three_turtle_eggs";
+                    case 4 -> "template_four_turtle_eggs";
+                    default -> throw new IllegalStateException("Unexpected egg count: " + eggs);
+                };
+
+                // Generate the model file with specified parent and texture under "all"
+                models().getBuilder(modelName)
+                        .parent(new ModelFile.UncheckedModelFile(mcLoc("block/" + parentModel)))
+                        .texture("all", modLoc("block/" + textureName));
+
+                // Add four rotations for the blockstate to handle specified eggs/hatch combination
+                variantBuilder.partialState()
+                        .with(DesertTurtleEggBlock.EGGS, eggs)
+                        .with(DesertTurtleEggBlock.HATCH, hatch)
+                        .addModels(
+                                new ConfiguredModel(models().getExistingFile(modLoc("block/" + modelName)), 0, 0, false),
+                                new ConfiguredModel(models().getExistingFile(modLoc("block/" + modelName)), 0, 90, false),
+                                new ConfiguredModel(models().getExistingFile(modLoc("block/" + modelName)), 0, 180, false),
+                                new ConfiguredModel(models().getExistingFile(modLoc("block/" + modelName)), 0, 270, false)
+                        );
+            }
+        }
+    }
+
 
     private void createFossilBlock(Block fossilBlock, String fossilName, String frontTexture, String sideTexture) {
         ResourceLocation name = new ResourceLocation(RealityMod.MOD_ID, fossilName);
