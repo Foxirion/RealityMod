d81d5335bbc7a8658749266d91cc0468fb54d807
Foxirion <dariustam@yahoo.com>
2024-10-10 14:38:36 +0300

    added resistance I for 5 seconds after taking damage and after that 20 seconds cooldown

--- src/main/java/net/foxirion/realitymod/event/ModEvents.java
+++ src/main/java/net/foxirion/realitymod/event/ModEvents.java
@@ -6,14 +6,20 @@ import net.foxirion.realitymod.block.ModBlocks;
 import net.foxirion.realitymod.entity.ModEntities;
 import net.foxirion.realitymod.entity.custom.DesertTurtleEntity;
 import net.foxirion.realitymod.item.ModItems;
+import net.foxirion.realitymod.item.custom.DesertTurtleHelmetItem;
+import net.minecraft.world.effect.MobEffectInstance;
+import net.minecraft.world.effect.MobEffects;
+import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.entity.SpawnPlacements;
 import net.minecraft.world.entity.npc.VillagerProfession;
 import net.minecraft.world.entity.npc.VillagerTrades;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Items;
 import net.minecraft.world.item.trading.MerchantOffer;
 import net.minecraft.world.level.levelgen.Heightmap;
 import net.minecraftforge.event.entity.SpawnPlacementRegisterEvent;
+import net.minecraftforge.event.entity.living.LivingHurtEvent;
 import net.minecraftforge.event.village.VillagerTradesEvent;
 import net.minecraftforge.event.village.WandererTradesEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
@@ -21,7 +27,7 @@ import net.minecraftforge.fml.common.Mod;
 
 import java.util.List;
 
-@Mod.EventBusSubscriber(modid = RealityMod.MOD_ID)
+@Mod.EventBusSubscriber(modid = RealityMod.MOD_ID, bus = Mod.EventBusSubscriber.Bus.FORGE)
 public class ModEvents {
 
     //Mob Spawning
@@ -63,6 +69,31 @@ public class ModEvents {
                 new ItemStack(ModBlocks.PALM_SAPLING.get().asItem(), 1),
                 8, 12, 0.2f));
 
+    }
+
+    //Desert Turtle Helmet effect
+    private static final String COOLDOWN_TAG = "DesertTurtleHelmetCooldown";
+
+    @SubscribeEvent
+    public static void onPlayerDamaged(LivingHurtEvent event) {
+        if (!(event.getEntity() instanceof Player player)) return;
+
+        // Check if player is wearing the helmet
+        ItemStack helmet = player.getItemBySlot(EquipmentSlot.HEAD);
+        if (!(helmet.getItem() instanceof DesertTurtleHelmetItem)) return;
+
+        // Get the current game time
+        long currentTime = player.level().getGameTime();
+        long lastActivated = helmet.getOrCreateTag().getLong(COOLDOWN_TAG);
+
+        // Check the cooldown (20 seconds in ticks = 400 ticks)
+        if (currentTime >= lastActivated + 400) {
+            // Apply Resistance I for 4 seconds (80 ticks)
+            player.addEffect(new MobEffectInstance(MobEffects.DAMAGE_RESISTANCE, 80, 0));
+
+            // Set the new cooldown time
+            helmet.getOrCreateTag().putLong(COOLDOWN_TAG, currentTime);
+        }
     }
 
 }
--- src/main/java/net/foxirion/realitymod/item/ModItems.java
+++ src/main/java/net/foxirion/realitymod/item/ModItems.java
@@ -4,6 +4,7 @@ import net.foxirion.realitymod.RealityMod;
 import net.foxirion.realitymod.block.ModBlocks;
 import net.foxirion.realitymod.entity.ModEntities;
 import net.foxirion.realitymod.entity.custom.ModBoatEntity;
+import net.foxirion.realitymod.item.custom.DesertTurtleHelmetItem;
 import net.foxirion.realitymod.item.custom.ModArmorMaterials;
 import net.foxirion.realitymod.item.custom.ModBoatItem;
 import net.minecraft.world.item.ArmorItem;
@@ -32,7 +33,7 @@ public class ModItems {
 
     // Desert Turtle
     public static final RegistryObject<Item> DESERT_TURTLE_HELMET = ITEMS.register("desert_turtle_helmet",
-            () -> new ArmorItem(ModArmorMaterials.DESERT_TURTLE, ArmorItem.Type.HELMET, new Item.Properties()));
+            () -> new DesertTurtleHelmetItem(ModArmorMaterials.DESERT_TURTLE, ArmorItem.Type.HELMET, new Item.Properties()));
     public static final RegistryObject<Item> DESERT_TURTLE_SCUTE = ITEMS.register("desert_turtle_scute",
             () -> new Item(new Item.Properties()));
 
